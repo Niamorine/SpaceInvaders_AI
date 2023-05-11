05a6487729fe949dcf65f09bc901bd3ef30457d8
yuanmengxin <125653583+yuanmengxin@users.noreply.github.com>
2023-05-11 17:54:01 +0200

    version with coordinate system

--- Neat/main.py
+++ Neat/main.py
@@ -3,10 +3,12 @@ import pygame
 import neat
 import os
 import pickle
+
+from SpaceInvaders_AI.Neat.parallele_et_matrices.main import vis_net
 from consts import WIDTH, HEIGHT
 from game import Game
 import visualize
-from svglib.svglib import svg2rlg
+# from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
 from PIL import Image
 import numpy as np
@@ -14,11 +16,11 @@ import numpy as np
 debug = False
 
 
-def svg2png():
-    drawing = svg2rlg('graph_best_so_far.svg')
-    renderPM.drawToFile(drawing, 'graph_best_so_far.png', fmt='PNG')
-    im = Image.open('graph_best_so_far.png')
-    im.show()
+# def svg2png():
+# drawing = svg2rlg('graph_best_so_far.svg')
+# renderPM.drawToFile(drawing, 'graph_best_so_far.png', fmt='PNG')
+# im = Image.open('graph_best_so_far.png')
+# im.show()
 
 
 class GameAi:
@@ -53,14 +55,9 @@ class GameAi:
                 if event.type == pygame.QUIT:
                     quit()
 
-            nearest_enemy_y = 0
-            nearest_enemy_x = 0
             laser_above = False
             nearest_laser_dist = (WIDTH ** 2 + HEIGHT ** 2) ** 0.5
-            nearest_laser_relative_x = WIDTH
-            nearest_laser_relative_y = HEIGHT
-            laser_relative_x = 0
-            laser_relative_y = 0
+
             if len(self.game.enemies) >= 1:
                 nearest_enemy_y = self.game.enemies[0].y + self.game.enemies[0].get_height() / 2
                 nearest_enemy_x = self.game.enemies[0].x + self.game.enemies[0].get_width() / 2
@@ -73,56 +70,45 @@ class GameAi:
                         laser_above = True
                     if dist < nearest_laser_dist:
                         nearest_laser_dist = dist
-                        nearest_laser_relative_x = laser_relative_x
-                        nearest_laser_relative_y = laser_relative_y
-            relative_enemy_y = nearest_enemy_y - self.game.player.y
-            relative_enemy_x = nearest_enemy_x - self.game.player.x
 
-            # Normalizing inputs
-            norm_relative_enemy_x = relative_enemy_x / WIDTH
-            norm_relative_enemy_y = relative_enemy_y / HEIGHT
-            norm_player_x = self.game.player.x / WIDTH
-            norm_player_y = self.game.player.y / HEIGHT
-            norm_nearest_laser_dist = nearest_laser_dist / ((WIDTH ** 2 + HEIGHT ** 2) ** 0.5)
             norm_cooldown = self.game.player.cool_down_counter / self.game.player.COOLDOWN
-            norm_nearest_laser_relative_x = nearest_laser_relative_x / WIDTH
-            norm_nearest_laser_relative_y = nearest_laser_relative_y / HEIGHT
 
-            tab_enemy_lasers = [[0 for i in range(1, 12)] for j in range(1, 41)]
+            tab_enemy_lasers = [0 for i in range(0, 30)]
+            j = 0
             for enemy in self.game.enemies:
                 for laser in enemy.lasers:
                     if laser.true_y > 0 and 0 < laser.true_x < WIDTH:
-                        case_x = int(laser.true_x * 40 / WIDTH) - 1
-                        case_y = int(laser.true_y * 11 / HEIGHT) - 1
+                        case_x = int(laser.true_x * 40 / WIDTH)
+                        case_y = int(laser.true_y * 11 / HEIGHT)
                         # print(case_x, case_y)
-                        tab_enemy_lasers[case_x][case_y] = 1
+                        tab_enemy_lasers[j] = case_x
+                        tab_enemy_lasers[j + 1] = case_y
+                        j = j + 2
                         # tab_enemy_lasers[case_x][case_y - 1] = 1
                         # tab_enemy_lasers[case_x][case_y + 1] = 1
 
-            tab_player_lasers = [[0 for i in range(1, 12)] for j in range(1, 41)]
+            tab_player_lasers = [0 for j in range(1, 41)]
             for laser in self.game.player.lasers:
                 laser_matrice_x = laser.true_x
                 laser_matrice_y = laser.true_y
-                case_x = int(laser.true_x * 40 / WIDTH) - 1
-                case_y = int(laser.true_y * 11 / HEIGHT) - 1
-                # print(case_x, case_y)
-                tab_player_lasers[case_x][case_y] = 1
-                tab_player_lasers[case_x][case_y - 1] = 1
-                tab_player_lasers[case_x][case_y + 1] = 1
+                case_x = int(laser.true_x * 40 / WIDTH)
+                tab_player_lasers[case_x] += 1
 
-            tab_enemy = [[0 for i in range(1, 6)] for j in range(1, 41)]
+            tab_enemy = [-1 for i in range(0, 20)]
+            j = 0;
             for enemy in self.game.enemies:
-                matrice_x = enemy.true_x
-                matrice_y = enemy.true_y
                 if 0 < enemy.true_y:
-                    case_x = int(enemy.true_x * 40 / WIDTH) - 1
-                    case_y = int(enemy.true_y * 5 / HEIGHT) - 1
+                    case_x = int(enemy.true_x * 40 / WIDTH)
+                    case_y = int(enemy.true_y * 5 / HEIGHT)
                     # print(case_x, case_y)
-                    tab_enemy[case_x][case_y] = 1
+                    if j < 20:
+                        tab_enemy[j] = case_x
+                        tab_enemy[j + 1] = case_y
+                        j = j + 2
 
-            tab_player = [0 for j in range(1, 41)]
-            case_x = int(self.game.player.true_x * 40 / WIDTH) - 1
-            tab_player[case_x] = 1
+            tab_player = [0 for j in range(0, 1)]
+            case_player = int(self.game.player.true_x * 40 / WIDTH) - 1
+            tab_player[0] = case_player
 
             tab_enemy_lasers = np.ndarray.flatten(np.array(tab_enemy_lasers))
             tab_player_lasers = np.ndarray.flatten((np.array(tab_player_lasers)))
@@ -149,7 +135,6 @@ class GameAi:
 
             self.game.loop()
 
-
     def train_ai(self, genome, c):
         run = True
         start_time = time.time()
@@ -164,76 +149,70 @@ class GameAi:
                         quit()
 
             prev_pos = self.game.player.x
-            nearest_enemy_y = 0
-            nearest_enemy_x = 0
-            laser_relative_x = 0
-            laser_relative_y = 0
             laser_above = False
-            nearest_laser_dist = (WIDTH ** 2 + HEIGHT ** 2) ** 0.5
-            nearest_laser_relative_x = WIDTH
-            nearest_laser_relative_y = HEIGHT
+
             if len(self.game.enemies) >= 1:
-                nearest_enemy_y = self.game.enemies[0].y + self.game.enemies[0].get_height()/2
-                nearest_enemy_x = self.game.enemies[0].x + self.game.enemies[0].get_width()/2
+                nearest_enemy_y = self.game.enemies[0].y + self.game.enemies[0].get_height() / 2
+                nearest_enemy_x = self.game.enemies[0].x + self.game.enemies[0].get_width() / 2
             for enemy in self.game.enemies:
                 for laser in enemy.lasers:
                     laser_relative_x = laser.true_x - self.game.player.true_x
                     laser_relative_y = laser.true_y - self.game.player.true_y
                     dist = (laser_relative_x ** 2 + laser_relative_y ** 2) ** 0.5
-                    if self.game.player.x <= laser.true_x - laser.true_width/2 and laser.true_x + laser.true_width/2 <= self.game.player.x + self.game.player.get_width() and self.game.player.y >= laser.true_y:
+                    if self.game.player.x <= laser.true_x - laser.true_width / 2 and laser.true_x + laser.true_width / 2 <= self.game.player.x + self.game.player.get_width() and self.game.player.y >= laser.true_y:
                         laser_above = True
-                    if dist < nearest_laser_dist:
-                        nearest_laser_dist = dist
-                        nearest_laser_relative_x = laser_relative_x
-                        nearest_laser_relative_y = laser_relative_y
-            relative_enemy_y = nearest_enemy_y - self.game.player.y
-            relative_enemy_x = nearest_enemy_x - self.game.player.x
 
             # Normalizing inputs
-            norm_relative_enemy_x = relative_enemy_x/WIDTH
-            norm_relative_enemy_y = relative_enemy_y/HEIGHT
-            norm_player_x = self.game.player.x/WIDTH
-            norm_player_y = self.game.player.y/HEIGHT
-            norm_nearest_laser_dist = nearest_laser_dist / ((WIDTH ** 2 + HEIGHT ** 2) ** 0.5)
             norm_cooldown = self.game.player.cool_down_counter / self.game.player.COOLDOWN
-            norm_nearest_laser_relative_x = nearest_laser_relative_x / WIDTH
-            norm_nearest_laser_relative_y = nearest_laser_relative_y /HEIGHT
 
-            tab_enemy_lasers = [[0 for i in range(1, 12)] for j in range(1, 41)]
+            tab_enemy_lasers = [0 for i in range(0, 30)]
+            j = 0
             for enemy in self.game.enemies:
                 for laser in enemy.lasers:
                     if laser.true_y > 0 and 0 < laser.true_x < WIDTH:
-                        case_x = int(laser.true_x * 40 / WIDTH) - 1
-                        case_y = int(laser.true_y * 11 / HEIGHT) - 1
+                        case_x = int(laser.true_x * 40 / WIDTH)
+                        case_y = int(laser.true_y * 11 / HEIGHT)
                         # print(case_x, case_y)
-                        tab_enemy_lasers[case_x][case_y] = 1
+                        tab_enemy_lasers[j] = case_x
+                        tab_enemy_lasers[j + 1] = case_y
+                        j = j + 2
                         # tab_enemy_lasers[case_x][case_y - 1] = 1
                         # tab_enemy_lasers[case_x][case_y + 1] = 1
 
-            tab_player_lasers = [[0 for i in range(1, 12)] for j in range(1, 41)]
+            tab_player_lasers = [0 for j in range(1, 41)]
             for laser in self.game.player.lasers:
                 laser_matrice_x = laser.true_x
                 laser_matrice_y = laser.true_y
-                case_x = int(laser.true_x * 40 / WIDTH) - 1
-                case_y = int(laser.true_y * 11 / HEIGHT) - 1
-                # print(case_x, case_y)
-                tab_player_lasers[case_x][case_y] = 1
-                tab_player_lasers[case_x][case_y - 1] = 1
-                tab_player_lasers[case_x][case_y + 1] = 1
+                case_x = int(laser.true_x * 40 / WIDTH)
+                tab_player_lasers[case_x] += 1
 
-            tab_enemy = [[0 for i in range(1, 6)] for j in range(1, 41)]
+            tab_enemy = [-1 for i in range(0, 20)]
+            j = 0;
             for enemy in self.game.enemies:
                 matrice_x = enemy.true_x
                 matrice_y = enemy.true_y
                 if 0 < enemy.true_y:
-                    case_x = int(enemy.true_x * 40 / WIDTH)-1
-                    case_y = int(enemy.true_y * 5 / HEIGHT)-1
+                    case_x = int(enemy.true_x * 40 / WIDTH)
+                    case_y = int(enemy.true_y * 5 / HEIGHT)
                     # print(case_x, case_y)
-                    tab_enemy[case_x][case_y] = 1
+                    if (j < 20):
+                        tab_enemy[j] = case_x
+                        tab_enemy[j + 1] = case_y
+                        j = j + 2
 
-            tab_player = [0 for j in range(1, 41)]
-            case_x = int(self.game.player.true_x * 40 / WIDTH) - 1
-            tab_player[case_x] = 1
+            i = 0
+            while i < 20:
+                if tab_enemy[i] != -1:
+                    j = tab_enemy[i]
+                    if tab_player_lasers[j] == 0:
+                        genome.fitness -= 0.02
+                    elif tab_enemy[i + 1] == 4:
+                        genome.fitness -= 0.1
+                i = i + 2
+
+            tab_player = [0 for j in range(0, 1)]
+            case_player = int(self.game.player.true_x * 40 / WIDTH) - 1
+            tab_player[0] = case_player
 
             tab_enemy_lasers = np.ndarray.flatten(np.array(tab_enemy_lasers))
             tab_player_lasers = np.ndarray.flatten((np.array(tab_player_lasers)))
@@ -251,24 +230,19 @@ class GameAi:
             match decision:
                 case 0:
                     self.shoot()
+                    genome.fitness += 0.01
                 case 1:
                     self.move_left()
                 case 2:
                     self.move_right()
                 case 3:
                     # penalty for not doing anything
-                    genome.fitness -= 0.01
-
-            # bonus for surviving
-            genome.fitness += 0.01
+                    # genome.fitness -= 0.1
+                    pass
 
             # penalty for being at the same position as before
             if self.game.player.x == prev_pos:
                 genome.fitness -= 0.03
-
-            # penalty for being under a laser
-            #if laser_above:
-             #   genome.fitness -= 2
 
             if debug:
                 count += 1
@@ -281,7 +255,6 @@ class GameAi:
                     count = 0
 
             game_info = self.game.loop()
-
 
             if game_info.lost:
                 genome.fitness += 50 * game_info.nb_enemies_killed
@@ -303,6 +276,7 @@ def eval_genomes(genomes, c):
     vis_net(c, view=False)
     # svg2png()
 
+
 def parallelEval(genome, c):
     draw = False
     human = False
@@ -321,17 +295,17 @@ def parallelEval(genome, c):
 
 
 def run_neat(c):
-    p = neat.Checkpointer.restore_checkpoint("neat-checkpoint-72")
+    p = neat.Checkpointer.restore_checkpoint("neat-checkpoint-78")
     # p = neat.Population(c)
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
     p.add_reporter(neat.Checkpointer(1))
-    pe = neat.ParallelEvaluator(11, parallelEval)
+    pe = neat.ParallelEvaluator(3, parallelEval)
 
     best = None
     # best = p.run(eval_genomes, 100)
-    best = p.run(pe.evaluate, 1)
+    best = p.run(pe.evaluate, 4)
 
     with open("best.pickle", "wb") as f:
         pickle.dump(best, f)
@@ -350,16 +324,16 @@ def test_ai(c):
     game.test_ai(best, c)
 
 
-def vis_net(c, view=True):
-    with open("best.pickle", "rb") as f:
-        best = pickle.load(f)
+# def vis_net(c, view=True):
+# with open("best.pickle", "rb") as f:
+#    best = pickle.load(f)
 
-    names = {-6: "laser_above", -5: "norm_relative_enemy_y", -4: "norm_relative_enemy_x",
-             -3: "norm_nearest_laser_relative_y", -2: "norm_nearest_laser_relative_x", -1: "norm_cooldown",
-             0: "shoot", 1: "left", 2: "right", 3: "nothing"}
-    # c.genome_config.input_keys = {-6: "-6", -5: "-5", -4: "-4", -3: "-3", -2: "-2", -1: "-1",
-    #                                   0: "shoot", 1: "left", 2: "right", 3: "nothing"}
-    visualize.draw_net(c, best, filename="graph_neural_network", view=view, node_names=names)
+# names = {-6: "laser_above", -5: "norm_relative_enemy_y", -4: "norm_relative_enemy_x",
+#         -3: "norm_nearest_laser_relative_y", -2: "norm_nearest_laser_relative_x", -1: "norm_cooldown",
+#         0: "shoot", 1: "left", 2: "right", 3: "nothing"}
+# c.genome_config.input_keys = {-6: "-6", -5: "-5", -4: "-4", -3: "-3", -2: "-2", -1: "-1",
+#                                   0: "shoot", 1: "left", 2: "right", 3: "nothing"}
+# visualize.draw_net(c, best, filename="graph_neural_network", view=view, node_names=names)
 
 
 if __name__ == "__main__":
@@ -370,23 +344,6 @@ if __name__ == "__main__":
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet,
                          neat.DefaultStagnation, config_path)
 
-    run_neat(config)
-    # test_ai(config)
+    #run_neat(config)
+    test_ai(config)
     # vis_net(config)
-
-    """
-    tab = [[0 for i in range(1,10)] for j in range(1,10)]
-    for enemy in self.game.enemies:
-                for laser in enemy.lasers:
-                    laser_matrice_x = laser.true_x - self.game.player.true_x + 100
-                    laser_matrice_y = laser.true_y - self.game.player.true_y + 100
-
-                    dist = (laser_relative_x ** 2 + laser_relative_y ** 2) ** 0.5
-                    case_x = (int)laser_matrice_x * 20
-                    case_y = (int)laser_matrice_y * 20
-                    tab[case_x][case_y] = 1;
-                    tab[case_x][case_y -1] = 1;
-                    tab[case_x][case_y+1]=1
-
-
-    """
